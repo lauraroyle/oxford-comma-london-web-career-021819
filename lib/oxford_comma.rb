9d6e382ae15250148c5f967e80216db66a822ccb
Laura Royle <lauraamie.royle@gmail.com>
2019-01-31 15:09:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
  if array.length == 1
-   array.join(", ")
+   array.join
+
+   #if array length is 1 it used join method to
 
  elsif array.length == 2
    array.join(" and ")
