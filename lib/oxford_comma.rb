53aa2835820761349f402cd4cd21598fda8f1e8d
Laura Royle <lauraamie.royle@gmail.com>
2019-01-31 15:18:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,10 +5,18 @@ def oxford_comma(array)
 
  elsif array.length == 2
    array.join(" and ")
-
+#if array has 2 elements it will create string and add 'and' in the middle where the comma separates
  else
    array << "and #{array.pop}"
    array.join(", ")
-
+# if array has more than 2 elements then we 1. use pop method to select last element (ie  "dragon fruits") 2. put 'and' before it and 3. use shovel method to add this to the end of the existing array
  end
 end
+
+#Step 1
+#["kiwi", "durian", "starfruit", "mangos", "dragon fruits"] << "and #{dragon fuits}"
+#["kiwi", "durian", "starfruit", "mangos", "and" "dragon fruits"]
+
+#Step 2
+#array.join(", ")
+#["kiwi, durian, starfruit, mangos, and, dragon fruits"]
