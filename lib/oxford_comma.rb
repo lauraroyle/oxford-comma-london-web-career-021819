589abe597b70d5692916e72204f0c128eb44f699
Laura Royle <lauraamie.royle@gmail.com>
2019-01-31 15:10:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
  if array.length == 1
    array.join
-
-   #if array length is 1 it used join method to
+   #if array length is 1 it used join method to create the string with no formatting
 
  elsif array.length == 2
    array.join(" and ")
